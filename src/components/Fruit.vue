ac97296e267a64814b0f0746479ea967a88b1c0f
TaeGeomDeveloper <alf6292@naver.com>
2022-04-23 19:30:10 +0900

    프로젝트 생성

---
+++ src/components/Fruit.vue
@@ -0,0 +1,23 @@
+<template>
+  <li>{{name}}?!</li>
+</template>
+
+
+<script>
+export default {
+  props: {
+    name: {
+        type: String,
+        default: ''
+    }
+  }
+}
+</script>
+
+// 유효범위 scoped
+<style scoped lang="scss">
+  h1 {
+    color: red !important;
+  }
+
+</style>
